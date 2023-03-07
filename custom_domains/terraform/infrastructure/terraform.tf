1cf3813a492857d43805837ad886dea3fc381e59
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2023-03-07 09:48:50 +0000

    Update azurerm provider to 3.46.0
    This will solve the issue of this item being added to terraform at each
    run:
    log_analytics_destination_type = "AzureDiagnostics"

--- custom_domains/terraform/infrastructure/terraform.tf
+++ custom_domains/terraform/infrastructure/terraform.tf
@@ -1,10 +1,9 @@
 terraform {
-
   required_version = "= 1.3.1"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.44.1"
+      version = "3.46.0"
     }
   }
   backend "azurerm" {
